a24fdbc23e3bea3c9be9b3fdb59f79603e46ad0c
o-shevaun <143563926+o-shevaun@users.noreply.github.com>
2024-10-03 18:04:10 +0300

    Add files via upload

---
+++ oop&pdo ass/login.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="css/styles.css">
+
+<form action="login.php" method="post">
+  <input type="text" name="username" placeholder="Username" required>
+  <input type="password" name="password" placeholder="Password" required>
+  <button type="submit">Login</button>
+</form>
---
+++ oop&pdo ass/signup.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/styles.css">
+
+<form action="register.php" method="post">
+  <input type="text" name="username" placeholder="Username" required>
+  <input type="email" name="email" placeholder="Email" required>
+  <input type="password" name="password" placeholder="Password" required>
+  <button type="submit">Sign Up</button>
+</form>
